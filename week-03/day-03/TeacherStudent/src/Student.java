8a686a91fe6ad98f4f0e1d3a7b09fb70929f9ad2
Pal_Dorka <pal.dorka.91@gmail.com>
2020-12-02 16:03:12 +0100

    added Teacher and Student

---
+++ week-03/day-03/TeacherStudent/src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public void learn() {
+        System.out.println("Learning something new...");
+    }
+
+    public void question(Teacher teacher) {
+        teacher.answer ();
+    }
+}
---
+++ week-03/day-03/TeacherStudent/src/Teacher.java
@@ -0,0 +1,9 @@
+public class Teacher {
+    public void teach(Student student) {
+        student.learn();
+    }
+
+    public void answer() {
+        System.out.println("Answering a question...");
+    }
+}
