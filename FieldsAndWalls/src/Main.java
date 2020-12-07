bc127da7d4c9798877bb5b80ce74a3dddbf2ee4b
Pal_Dorka <pal.dorka.91@gmail.com>
2020-12-07 17:35:33 +0100

    FieldsAndWalls

--- FieldsAndWalls/src/Main.java
+++ FieldsAndWalls/src/Main.java
@@ -31,24 +31,34 @@ public class Main {
 
     private static int[][] floodFillCheck(int[][] table) {
         table[0][0] = 0;
+        int numberOfCorrectFields = 0;
         for (int i = 0; i < table.length; i++) {
             for (int j = 0; j < table.length; j++) {
-                if (table[i][j] == 0) {
-                    continue;
-                }
-                boolean isFloodPossible = true;
 
             }
-
         }
-
         return table;
     }
 
-    private static boolean ableToContinue(int i, int j, int[][] table) {
-        if ((j >= 0) && ((j) <= 10) && (table[i][j]) == 0) {
+    private static boolean isItAFreeField(int i, int j, int[][] table) {
+        if ((j >= 0) && ((j) <= 10) && (i >= 0) && ((i) <= 10) && (table[i][j]) == 0) {
             return true;
         }
         return false;
+    }
+
+    private static int continueToNextField(int i, int j, int[][] table) {
+        if (!(isItAFreeField(i, j - 1, table) && isItAFreeField(i, j + 1, table)
+                && isItAFreeField(i + 1, j, table) && isItAFreeField(i - 1, j, table))) {
+            int counter = 1;
+            return counter;
+        }
+
+        isItAFreeField(i, j - 1, table) {
+            isItAFreeField(i, j - 1, table);
+        }
+                    ||isItAFreeField(i, j + 1, table)
+                || isItAFreeField(i - 1, j, table) || isItAFreeField(i + 1, j, table) ){
+        }
     }
 }
