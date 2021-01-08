71ffd1a95af2cf0646db30362f73b38293d5c9fb
Pal_Dorka <pal.dorka.91@gmail.com>
2021-01-08 11:56:01 +0100

    beginnings of the webshop

---
+++ week-06/day-05/webshop/src/main/java/com/greenfoxacademy/webshop/controllers/WebShopController.java
@@ -0,0 +1,55 @@
+package com.greenfoxacademy.webshop.controllers;
+
+import com.greenfoxacademy.webshop.models.ItemList;
+import com.greenfoxacademy.webshop.models.ShopItem;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class WebShopController {
+
+    ItemList itemList = new ItemList();
+
+    @GetMapping("/webshop")
+    public String webshop(Model model){
+        model.addAttribute("itemList", itemList.getItemList());
+        return "index";
+    }
+
+    @GetMapping("/only-available")
+    public String onlyAvailable(Model model){
+        List<ShopItem> availableItemList = itemList.getItemList().stream()
+                .filter(item -> item.getQuantityOfStock()>0)
+                .collect(Collectors.toList());
+        model.addAttribute("itemList", availableItemList);
+        return "index";
+    }
+
+    @GetMapping("/cheapest-first")
+    public String cheapestFirst(Model model){
+        model.addAttribute("itemList", itemList.getItemList());
+        return "index";
+    }
+
+    @GetMapping("/contains-nike")
+    public String containsNike(Model model){
+        model.addAttribute("itemList", itemList.getItemList());
+        return "index";
+    }
+
+    @GetMapping("/average-stock")
+    public String averageStock(Model model){
+        model.addAttribute("itemList", itemList.getItemList());
+        return "index";
+    }
+
+    @GetMapping("/most-expensive-available")
+    public String mostExpensiveAvailable(Model model){
+        model.addAttribute("itemList", itemList.getItemList());
+        return "index";
+    }
+}
---
+++ week-06/day-05/webshop/src/main/java/com/greenfoxacademy/webshop/models/ShopItem.java
@@ -0,0 +1,35 @@
+package com.greenfoxacademy.webshop.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShopItem {
+
+    private String name;
+    private String description;
+    private int price;
+    private int quantityOfStock;
+
+    public ShopItem(String name, String description, int price, int quantityOfStock) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantityOfStock = quantityOfStock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantityOfStock() {
+        return quantityOfStock;
+    }
+}
